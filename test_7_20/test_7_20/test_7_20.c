b186feded045f55266b21e2bffd91940d6b5d968
itzzan <2947688519@qq.com>
2022-07-20 16:32:22 +0800

    学习C语言

---
+++ test_7_20/test_7_20/test_7_20.c
@@ -0,0 +1,62 @@
+
+
+#include <stdio.h>
+//int main()
+//{
+//	//printf("c:\test\test.c");
+//	printf("ab\ncd");//  \+n
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	//printf("(are you ok\?\?)"); // ??) -- ] - 三字母词
+//	//(are you ok]
+//	//
+//	//printf("%c\n", '\'');
+//	printf("%s\n", "abc");
+//	printf("%s\n", "a");
+//	printf("%s\n", "\"");
+//	printf("c:\\test\\test.c");
+//	printf("\a\a\a");
+//	printf("?");
+//
+//	//printf 在打印数据的时候，可以指定打印的格式
+//
+//
+//
+//	return 0;
+//}
+
+//printf("%d", 100);
+//printf("%c", 'a');
+//printf("%s", "abc");
+
+
+//int main()
+//{
+//	//printf("%c\n", '\130');// 8进制的130是十进制的多少呢？
+//	// X == ASCII码值是88
+//	//printf("%c\n", '\101'); // A - 65 - 8进制是：101
+//	//printf("%c\n", '\x30'); // 48 - '0'
+//	//printf("%d\n", strlen("abc")); // 3
+//
+//	printf("%d\n", strlen("c:\test\328\test.c"));
+//
+//	return 0;
+//}
+
+//注释是用来解释复杂代码的
+//
+
+int main()
+{
+	//int a = 10;  // C++的注释风格
+
+	/*
+	int b = 0;
+	*/   //C语言的注释风格 - 不支持嵌套注释
+
+	return 0;
+}
