5345636e57b179e58618d053f3f172132dac5e7a
itzzan <2947688519@qq.com>
2022-08-16 21:39:40 +0800

    day28

---
+++ test_8_16_2/test_8_16_2/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//ÉùÃ÷Íâ²¿·ûºÅ
+extern int Add(int x, int y);
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int ret = Add(a, b);
+
+	return 0;
+}
